7da7f2522178c9869df47cb249733fc5e86c1164
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2024-09-29 23:16:05 -0600

    initial commit

---
+++ 1-SingleResponsability/Program.cs
@@ -0,0 +1,6 @@
+﻿using SingleResponsability;
+
+StudentRepository studentRepository = new();
+ExportHelper exportHelperCsv = new();
+exportHelperCsv.ExportStudents(studentRepository.GetAll());
+Console.WriteLine("Proceso Completado");
